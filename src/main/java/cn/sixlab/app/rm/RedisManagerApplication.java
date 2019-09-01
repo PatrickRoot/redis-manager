fb74e20eceb690550470a034045316d4c4257dab
Patrick <nianqinianyi@163.com>
2019-09-01 14:24:47 +0800

    很久以前的代码未提交

--- src/main/java/cn/sixlab/app/rm/RedisManagerApplication.java
+++ src/main/java/cn/sixlab/app/rm/RedisManagerApplication.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.clients.jedis.Jedis;
 
 import java.io.IOException;
 
@@ -25,6 +26,8 @@ public class RedisManagerApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        test();
+        
         Platform.setImplicitExit(true);
         Parent parent;
         try {
@@ -37,5 +40,45 @@ public class RedisManagerApplication extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("Redis Manager");
         primaryStage.show();
+    }
+    
+    public String test() {
+    
+        Jedis jedis = new Jedis("192.168.1.12", 32768);
+        // 直接调用jedis对象的方法，方法名称和redis的命令一致
+        jedis.set("key1", "test01");
+        jedis.set("key2", "test02");
+        String key1 = jedis.get("key1");
+        String key2 = jedis.get("key2");
+        System.out.println(key1 + " " + key2);
+        // 关闭jedis
+        jedis.close();
+    
+        //RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        //configuration.setDatabase(0);
+        //configuration.setHostName("localhost");
+        //configuration.setPort(32768);
+        //
+        //RedisConnectionFactory factory = new JedisConnectionFactory(configuration);
+        //
+        //RedisConnection connection = factory.getConnection();
+        //
+        //System.out.println(connection.isClosed());
+        //System.out.println(connection.isPipelined());
+        //System.out.println(connection.isQueueing());
+        //System.out.println(connection.isSubscribed());
+        //
+        //RedisTemplate template = new RedisTemplate();
+        //
+        //template.setConnectionFactory(factory);
+        //
+        //ValueOperations operations = template.opsForValue();
+        //
+        //operations.set("abc","1");
+        //System.out.println(operations.get("abc"));
+        //System.out.println(template.delete("abc"));
+        //
+        return "template";
+    
     }
 }
